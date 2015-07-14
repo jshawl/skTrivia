4fb199c4b5903f1191e8267f577d35e236c42988
Samuel Key <s.key08@gmail.com>
2015-07-14 11:13:17 -0400

    MVP, trivia is working

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,96 @@
 $(document).ready(function(){
 
 var startButton = $( "#start" );
-var submitButton = $( "#submit" )
+var submitButton = $( "button.submit" );
+var score = $("h3 .score")
+var numberQs = $(".questions").children().length;
+
+
+score = 0;
 
 startButton.on("click", function(){
-  $( ".questions" ).css( "display", "block" );
+  $( ".q1.xx" ).css( "display", "block" );
 });
 
 startButton.on("click", function(){
-  $( "#start" ).css( "display", "none" );
+  $("#start").css("display", "none");
 });
 
-startButton.on("click", function(){
-  $( "#submit" ).css( "display", "block" );
-});
+$("#next.next1").on("click", function(){
+  $(".q1.xx").css("display", "none");
+  $(".q2.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
 
-// var nextQuestion = submitButton.on("click", function(){
+$("#next.next2").on("click", function(){
+  $(".q2.xx").css("display", "none");
+  $(".q3.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+$("#next.next3").on("click", function(){
+  $(".q3.xx").css("display", "none");
+  $(".q4.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+$("#next.next4").on("click", function(){
+  $(".q4.xx").css("display", "none");
+  $(".q5.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+$("#next.next5").on("click", function(){
+  $(".q5.xx").css("display", "none");
+  $(".q6.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+$("#next.next6").on("click", function(){
+  $(".q6.xx").css("display", "none");
+  $(".q7.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+$("#next.next7").on("click", function(){
+  $(".q7.xx").css("display", "none");
+  $(".q8.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+$("#next.next8").on("click", function(){
+  $(".q8.xx").css("display", "none");
+  $(".q9.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+$("#next.next9").on("click", function(){
+  $(".q9.xx").css("display", "none");
+  $(".q10.xx").css("display", "block");
+  $("div .right, div .wrong").css("display", "none")
+})
+
+var checkAnswer = function() {
+    if($(this).parent().find('input[value=correct]').is(":checked") ) {
+      $(this).prop('disabled', true);
+      $("div .right").css("display", "inline-block")
+      // alert('Correct! +1');
+      score++
+     } else {
+      $(this).prop('disabled', true);
+      $("div .wrong").css("display", "inline-block")
+    //   alert('Sorry...');
+      score--
+    }
+    $(".score").html(score)
+};
+
+submitButton.on("click", checkAnswer);
+
+// var nextQuestion = function(){
 //   $( ".questions .q1" ).css( "display", "none" );
 // $( ".questions .q2" ).css( "display", "block" );
 // });
-
-submitButton.on("click", function() {
-  if($('input[value=correct]').is(":checked") ) {
-    alert('Correct! +1');
-  } else {
-    alert('Sorry...');
-  }
-});
-
-
-
-
-
 
 
 
