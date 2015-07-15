2d3b5ab8472841e2faa46e63285e9c3cd5635679
Samuel Key <s.key08@gmail.com>
2015-07-14 22:25:00 -0400

    everything working

--- js/scripts.js
+++ js/scripts.js
@@ -4,70 +4,49 @@ var startButton = $( "#start" );
 var submitButton = $( "button.submit" );
 var score = $("h3 .score")
 var numberQs = $(".questions").children().length;
-
-
+var next = $("#next");
 score = 0;
 
+//
+var timer;
+seconds = 60;
 startButton.on("click", function(){
-  $( ".q1.xx" ).css( "display", "block" );
-});
+  timer = setInterval(updateTime, 1000);
+})
+function updateTime(){
+  seconds--;
+  $("#timer").html(seconds)
+if(seconds === 0){
+  alert("Time's Up!")
+  clearInterval(seconds)
+  $(".questions").css("display", "none")
+  $("#timer").css("display", "none")
+}
+}
+//
 
 startButton.on("click", function(){
+  $(".q1.xx").css( "display", "block" );
   $("#start").css("display", "none");
 });
 
-$("#next.next1").on("click", function(){
-  $(".q1.xx").css("display", "none");
-  $(".q2.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
+// startButton.on("click", function(){
+//   $("#start").css("display", "none");
+// });
+
+$(".next").on("click", function(){
+  var form = $(this).parent();
+  form.hide();
+  form.next().show();
 })
 
-$("#next.next2").on("click", function(){
-  $(".q2.xx").css("display", "none");
-  $(".q3.xx").css("display", "block");
+$("div .next").on("click", function(){
   $("div .right, div .wrong").css("display", "none")
-})
+});
 
-$("#next.next3").on("click", function(){
-  $(".q3.xx").css("display", "none");
-  $(".q4.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
-})
-
-$("#next.next4").on("click", function(){
-  $(".q4.xx").css("display", "none");
-  $(".q5.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
-})
-
-$("#next.next5").on("click", function(){
-  $(".q5.xx").css("display", "none");
-  $(".q6.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
-})
-
-$("#next.next6").on("click", function(){
-  $(".q6.xx").css("display", "none");
-  $(".q7.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
-})
-
-$("#next.next7").on("click", function(){
-  $(".q7.xx").css("display", "none");
-  $(".q8.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
-})
-
-$("#next.next8").on("click", function(){
-  $(".q8.xx").css("display", "none");
-  $(".q9.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
-})
-
-$("#next.next9").on("click", function(){
-  $(".q9.xx").css("display", "none");
-  $(".q10.xx").css("display", "block");
-  $("div .right, div .wrong").css("display", "none")
+$("#finish.finish10").on("click", function(){
+  $("div .q10").css("display", "none")
+  $("h2").css("display", "none")
 })
 
 var checkAnswer = function() {
@@ -86,13 +65,6 @@ var checkAnswer = function() {
 };
 
 submitButton.on("click", checkAnswer);
-
-// var nextQuestion = function(){
-//   $( ".questions .q1" ).css( "display", "none" );
-// $( ".questions .q2" ).css( "display", "block" );
-// });
-
-
 
 
 });
